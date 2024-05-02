f22f5b309e951ea0651567c0b90b961e8113685d
yuyashirane <165985156+yuyashirane@users.noreply.github.com>
2024-05-02 09:02:42 +0900

    Update bmi.js

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,6 +1,8 @@
 // 変数の宣言・値の代入
 let userWeight = 68;
 let userHight = 1.7;
+let userBMI;
 
 // BMIの計算
-console.log(userWeight / (userHight * userHight));
+userBMI = userWeight / (userHight * userHight);
+console.log(userBMI);
